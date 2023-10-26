effa7df5f3209179f87677aa3d97499a533b53f9
timjozi911 <145546553+timjozi911@users.noreply.github.com>
2023-10-26 17:44:48 +0200

    feat: set up player and enemy surfaces

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import pygame
 from sys import exit
+from const import SCREEN_HEIGHT, SCREEN_WIDTH
 
 pygame.init()
 
-WIDTH = 800
-HEIGHT = 400
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('NeoZa game')
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+sky_surf = pygame.image.load('graphics/sky.png').convert()
+ground_surf = pygame.image.load('graphics/ground.png').convert()
+text_surf = test_font.render('My Game',False,'Black')
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600, 300))
+
+player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 300)) 
 
 while True:
     for event in pygame.event.get():
-        if event == pygame.QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
+    screen.blit(sky_surf, (0,0))
+    screen.blit(ground_surf, (0,300))
+    screen.blit(text_surf, (300, 50))
+
+    snail_rect.x -= 3
+    if snail_rect.right <= 0: snail_rect.left = 800
+
+    screen.blit(snail_surf, snail_rect)
+    screen.blit(player_surf, player_rect)
+
     pygame.display.update()
     clock.tick(60)
-    
